0af925281c2357a73b67e48b9178b94a2d94b4aa
Muhammad Ibrahim Basra Cheema <pakbarca21@gmail.com>
2025-12-04 23:17:05 +0000

    Fixed minor design issue

--- laravel/resources/views/components/text-input.blade.php
+++ laravel/resources/views/components/text-input.blade.php
@@ -1,3 +1,3 @@
 @props(['disabled' => false])
 
-<input @disabled($disabled) {{ $attributes->merge(['class' => 'border-gray-300 dark:border-gray-700 dark:bg-gray-900 dark:text-gray-300 focus:border-indigo-500 dark:focus:border-indigo-600 focus:ring-indigo-500 dark:focus:ring-indigo-600 rounded-md shadow-sm']) }}>
+<input @disabled($disabled) {{ $attributes->merge(['class' => 'border-gray-300 dark:border-gray-700 dark:bg-gray-900 dark:text-gray-300 focus:border-[#7FA82E] dark:focus:border-[#7FA82E] focus:ring-[#7FA82E] dark:focus:ring-[#7FA82E] rounded-md shadow-sm']) }}>
